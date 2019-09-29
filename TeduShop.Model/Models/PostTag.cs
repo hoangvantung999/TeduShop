49dee3bfb91c71f73bf649fbbcd66c4610442f21
hoangthanhtung <hoangvantung999@gmail.com>
2019-09-29 15:13:20 +0700

    change to Models

--- TeduShop.Model/Models/PostTag.cs
+++ TeduShop.Model/Models/PostTag.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeduShop.Model.Model
+namespace TeduShop.Model.Models
 {
     [Table("PostTags")]
     public class PostTag
