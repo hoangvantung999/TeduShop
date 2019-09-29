84de67850b4a65af190093929aea0261148dd321
hoangthanhtung <hoangvantung999@gmail.com>
2019-09-29 09:41:54 +0700

    changed to engineer

--- TeduShop.Web/Controllers/HomeController.cs
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace TeduShop.Web.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact erereer page.";
+            ViewBag.Message = "Your contact engirneer page.";
 
             return View();
         }
